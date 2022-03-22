a3412ff14154df71f8fe529fb4de9db374aa27d9
Hitarthi Gandhi <hitarthi.gandhi@cgi.com>
2022-03-22 11:56:17 -0600

    success

---
+++ Controller/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ProducerAPI.Models;
+using System.Threading.Tasks;
+
+namespace ProducerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        public OrdersController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Order order)
+        {
+            string message = JsonConvert.SerializeObject(order);
+            ProducerConfig config = new ProducerConfig
+            {
+                BootstrapServers = configuration["Kafka:Server"]
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var result = await producer.ProduceAsync("test", new Message<Null, string>
+                {
+                    Value = message
+                });
+                return await Task.FromResult(Ok("Message Sent"));
+            };
+        }
+    }
+}
